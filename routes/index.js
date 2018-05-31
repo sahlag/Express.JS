e7678a479ada8d8b74eaeb8526893ca11c8a6711
sahlag <boussaid-souheil@live.fr>
2018-05-31 12:05:45 +0200

    passage du variables aux vues

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index',{
+    title: "Page d'accueil",
+    name:"souheil"
+
+  });
+  
+});
+router.get('/accueil2', function(req, res, next) {
+  res.render('index',{
+    name:"souheil"
+
+  });
+  
 });
 
 module.exports = router;
