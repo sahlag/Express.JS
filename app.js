68402b721c18f1411708b956be3c77e9fc233187
sahlag <boussaid-souheil@live.fr>
2018-05-31 16:18:56 +0200

    Mongoos: installation et connexion

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+//on importe les modumles dont a besoin
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let mongoose = require('mongoose');
 
+// on importe les différent routeur
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let productRouter = require ('./routes/products');
 
+// on céé notre application Express
 let app = express();
 
-// D'finition du moteur de vues
+// D'finition du moteur de templates(vues)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -19,16 +23,30 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// rendre des assets: CSS/JS/IMG/FONTS.
+
+// rendre des assets statiques: CSS/JS/IMG/FONTS.
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dist', express.static(path.join(__dirname,'node_modules/jquery/dist')));
 app.use('/dist', express.static(path.join(__dirname,'node_modules/bootstrap/dist')));
 
+//Mongoose
+console.log('version de mongoose: ' +mongoose.version);
+
+mongoose.connect('mongodb://localhost/food');
+let database = mongoose.connection;
+database.on('error',(err) =>{
+  console.log('Erreur avec mongoose');
+  console.log(err);
+});
+database.once('open',()=> console.log('Connextion réussie avec MongoDB'));
 //Systéme de routage
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/produit',productRouter);
-
+/**
+ * Route définies à la main
+ * a exporter par la suite dans un fichier séparé
+ */
 //création d'un route avec GET
 app.get('/contact',(req, res) =>{
 res.send("<h1>bienvenue sur la page de contact</h1>")
@@ -46,12 +64,12 @@ app.get(/(ba)+r+$/,(req, res)=>{
   res.send('URL catched !');
 });
 
-// catch 404 and forward to error handler
+// gestion de l'erreur 404.
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Middleware d'erreur: exécuté quand il ya une erreur
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
